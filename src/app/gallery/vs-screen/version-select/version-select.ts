e771a622bef6432ceceb82563dd49e568516cdf5
GaryButternubs <GaryButternubs123@gmail.com>
2025-06-23 18:30:58 -0400

    Added forgotten component to vs-screen parent component.

---
+++ src/app/gallery/vs-screen/version-select/version-select.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-version-select',
+  imports: [],
+  templateUrl: './version-select.html',
+  styleUrl: './version-select.scss'
+})
+export class VersionSelect {
+
+}
