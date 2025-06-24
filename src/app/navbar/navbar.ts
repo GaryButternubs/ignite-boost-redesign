a4b73a526615f529ea408857b7d2a4c0d3a2e0a5
GaryButternubs <GaryButternubs123@gmail.com>
2025-06-24 17:41:14 -0400

    Changed favicon.

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,43 +1,25 @@
-import { Component, inject, ViewChild } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatTreeModule } from '@angular/material/tree';
 import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   imports: [
+    RouterLink,
+    NgOptimizedImage,
     MatToolbarModule,
     MatButtonModule,
-    MatSidenavModule,
     MatListModule,
     MatMenuModule,
-    MatTreeModule,
     MatIconModule,
-    RouterLink,
-    AsyncPipe,
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  // Identify if device meets mobile screen-sizes and should use sidenav menu
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  // TO-DO: Read login state from session and change options from "Login" and "Signup" to "Signout"
-  loggedIn = false;
 }
