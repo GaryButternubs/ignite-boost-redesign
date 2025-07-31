819c75786416d0a27159ba04d39d69846ec1287c
GaryButternubs <GaryButternubs123@gmail.com>
2025-07-31 17:10:00 -0400

    Created files for test API requests, along with data.

---
+++ src/app/api/api-requests.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiRequests {
+  private delay = 1000; // Simulated network delay
+
+  constructor() {
+    
+   }
+}
