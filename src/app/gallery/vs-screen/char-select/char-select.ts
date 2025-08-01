918f4d6ad28babfaf2ee67406fe67e10a58fedf0
GaryButternubs <GaryButternubs123@gmail.com>
2025-08-01 12:05:29 -0400

    Fixed mistake where Char-Select code was written in Player-Search component.

--- src/app/gallery/vs-screen/char-select/char-select.ts
+++ src/app/gallery/vs-screen/char-select/char-select.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Character, PlayableCharacters } from '../../../api/Characters';
 
 @Component({
   selector: 'app-char-select',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './char-select.scss'
 })
 export class CharSelect {
+  isIgnition = input<boolean>(true);
+  isP1 = input<boolean>(true); // Affects skew direction 
+  updateCharQuery = output<string>();
 
+  selectedChar = signal<Character>(PlayableCharacters[10]); // All characters
+
+  updateDisplayedChar(char: Character) {
+    this.selectedChar.set(char);
+
+    // Null-ish coalescing multiple times because I suck
+    this.updateCharQuery.emit(char.internal ?? char.short ?? char.name);
+  }
 }
--- src/app/gallery/vs-screen/player-search/player-search.ts
+++ src/app/gallery/vs-screen/player-search/player-search.ts
@@ -8,16 +8,5 @@ import { Character, PlayableCharacters } from '../../../api/Characters';
   styleUrl: './player-search.scss'
 })
 export class PlayerSearch {
-  isIgnition = input<boolean>(true);
-  isP1 = input<boolean>(true); // Affects skew direction 
-  updatePlayerQuery = output<string>();
-
-  selectedChar = signal<Character>(PlayableCharacters[10]); // All characters
-
-  updateDisplayedPlayer(char: Character) {
-    this.selectedChar.set(char);
-
-    // Null-ish coalescing multiple times because I suck
-    this.updatePlayerQuery.emit(char.internal ?? char.short ?? char.name);
-  }
+  
 }
