91451285ff851c96509ba6c48302dca1e84880cb
GaryButternubs <GaryButternubs123@gmail.com>
2025-08-20 10:28:25 -0400

    Adjusted positioning and styling of character-menu to fit on one line within 1280 pixels.

--- src/app/gallery/vs-screen/player/assist-select/assist-menu/assist-menu.ts
+++ src/app/gallery/vs-screen/player/assist-select/assist-menu/assist-menu.ts
@@ -1,10 +1,11 @@
 import { Component, input, output, signal } from '@angular/core';
 import { NgOptimizedImage, NgClass } from '@angular/common';
 import { Character, AssistCharacters } from '../../../../../api/Characters';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-assist-menu',
-  imports: [NgOptimizedImage, NgClass],
+  imports: [NgOptimizedImage, NgClass, MatProgressSpinnerModule],
   templateUrl: './assist-menu.html',
   styleUrl: './assist-menu.scss'
 })
@@ -12,6 +13,7 @@ export class AssistMenu {
   // Only displays when viewport width >= 1280px
   isIgnition = input<boolean>(true);
   isP1 = input<boolean>(true);
+  isOpen = input<boolean>(false);
   updateSelectedAsst = output<Character>();
   assistCharacters = AssistCharacters;
 
