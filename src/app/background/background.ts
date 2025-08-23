801c2ed1922bafa1b76032cedbaa845541df5264
GaryButternubs <GaryButternubs123@gmail.com>
2025-08-23 16:09:12 -0400

    Removed unneeded dependency from background component.

--- src/app/background/background.ts
+++ src/app/background/background.ts
@@ -1,11 +1,10 @@
 import { Component, input } from '@angular/core';
-import { NgClass } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Code } from './code/code';
 
 @Component({
   selector: 'app-background',
-  imports: [RouterOutlet, NgClass, Code],
+  imports: [RouterOutlet, Code],
   templateUrl: './background.html',
   styleUrl: './background.scss'
 })
